0aca19374d5fcd1e1fe70505538cdb80f3b576c6
Helen Pickavance <helen.pickavance@digital.cabinet-office.gov.uk>
2025-12-15 11:17:33 +0000

    Test migration for use on Integration

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_12_01_174030) do
+ActiveRecord::Schema[8.0].define(version: 2025_12_15_111224) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension "pg_catalog.plpgsql"
 
   create_table "placeholder", force: :cascade do |t|
     t.text "placeholder"
+  end
+
+  create_table "tests", force: :cascade do |t|
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 end
