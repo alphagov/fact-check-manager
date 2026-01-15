7aadc08955a842d81c957b06fb3b0e0f5a54913e
Helen Pickavance <helen.pickavance@digital.cabinet-office.gov.uk>
2026-01-15 14:17:36 +0000

    Add Request model testing
    - Factories and tests

---
+++ spec/factories/request.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :request do
+    edition_id { SecureRandom.uuid }
+    requester_name { "Malcolm Tucker" }
+    requester_email { "m.tucker@gov.uk" }
+    current_content { "<HTML> Changes go here <HTML>" }
+    deadline { Time.zone.now + 1.week }
+  end
+end
