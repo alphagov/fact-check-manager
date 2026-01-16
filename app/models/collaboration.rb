a3c087077fe861818141080cba274cb0c8919846
Helen Pickavance <helen.pickavance@digital.cabinet-office.gov.uk>
2026-01-16 16:15:02 +0000

    Add Collaboration model testing
    - Collaborations factory with associated models and required attribute

--- app/models/collaboration.rb
+++ app/models/collaboration.rb
@@ -3,4 +3,8 @@ class Collaboration < ApplicationRecord
   belongs_to :user
 
   validates :role, presence: true
+  validates :user_id, uniqueness: {
+    scope: :request_id,
+    message: "is already a collaborator on this request",
+  }
 end
