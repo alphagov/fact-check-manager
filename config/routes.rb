8bbb41e10fc70911b370c2a1477cd168ad6e6d9a
Barbara <barbara.slawinska@digital.cabinet-office.gov.uk>
2026-01-20 17:02:29 +0000

    Add /api/requests endpoint for Publisher fact check requests

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   get "/healthcheck/ready", to: GovukHealthcheck.rack_response
 
   root to: "application#hello_world"
+
+  namespace :api do
+    resources :requests, only: [:create]
+  end
 end
