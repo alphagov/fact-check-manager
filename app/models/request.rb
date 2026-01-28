b8ec25a1fcc807189291cd94b99813a10e03c9ac
Helen Pickavance <helen.pickavance@digital.cabinet-office.gov.uk>
2026-01-28 16:29:18 +0000

    Add scopes to simplify fetching the latest request for a particular edition

--- app/models/request.rb
+++ app/models/request.rb
@@ -4,4 +4,7 @@ class Request < ApplicationRecord
   has_one :response
 
   validates :edition_id, :requester_name, :requester_email, :status, :current_content, :deadline, presence: true
+  scope :for_edition, ->(edition_id) { where(edition_id: edition_id) }
+  scope :most_recent_first, -> { order(created_at: :desc) }
+  scope :most_recent_for_edition, ->(edition_id) { for_edition(edition_id).most_recent_first.first }
 end
