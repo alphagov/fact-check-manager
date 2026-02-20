183c4de17bc0e053db9f23fc84f5e181f787dcaa
Helen Pickavance <helen.pickavance@digital.cabinet-office.gov.uk>
2026-02-20 13:49:51 +0000

    WIP Fix text to json data conversion issue
    - For previous_content and current_content fields:
    - Create a new json column to use to convert the old data into
    - Remove old text columns
    - Rename json columns to replace old text columns

--- spec/factories/request.rb
+++ spec/factories/request.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
     source_app { "publisher" }
     requester_name { "Malcolm Tucker" }
     requester_email { "m.tucker@gov.uk" }
-    current_content { "<HTML> Changes go here <HTML>" }
+    current_content { { "body" => "<HTML> Changes go here <HTML>" } }
     deadline { Time.zone.now + 1.week }
   end
 end
