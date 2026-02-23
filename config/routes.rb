d822b57474e2b15114f6eb33ac0ee20c0458c0e7
Jonathon <jonathon.shire@digital.cabinet-office.gov.uk>
2026-02-23 13:49:31 +0000

    Implement fact check confirmation flow
    A relatively simple confirmation flow. In this iteration, the flow is standalone
    with no link to a document or API hook, but ready for both to be plugged
    in with minimal effort.
    
    This iteration only handles the "happy" path, however has much of the backend
    in place for the "unhappy" (there are needed changes) path due to the nature
    of the two tasks.

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,17 @@ Rails.application.routes.draw do
 
   get "compare", to: "fact_check_comparison#compare"
 
+  # TODO: Wrap in a resources block
+  # See: https://github.com/alphagov/fact-check-manager/pull/33#discussion_r2905663106
+  get  "respond", to: "fact_check_response#respond_to_fact_check"
+  post "respond", to: "fact_check_response#verify_fact_check_response"
+
+  get  "confirm_response", to: redirect("/respond")
+  post "confirm_response", to: "fact_check_response#send_response"
+
+  get  "fact_check_submitted", to: redirect("/respond")
+  post "fact_check_submitted", to: "fact_check_response#fact_check_submitted"
+
   namespace :api do
     resources :requests, only: [:create]
   end
