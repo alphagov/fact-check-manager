23ce1939e91b0cab67bc53d23c5ff45a439d038a
Helen Pickavance <helen.pickavance@digital.cabinet-office.gov.uk>
2026-02-24 12:49:38 +0000

    Update DB and model to handle content hashes containing only string values - Migration to create new json column types and convert existing data from text to json - Migration to remove old text columns and rename new json ones to replace old text ones - Ensure previous and current content fields are validated
    Merge commit

--- app/models/request.rb
+++ app/models/request.rb
@@ -4,7 +4,27 @@ class Request < ApplicationRecord
   has_one :response
 
   validates :source_id, :source_app, :requester_name, :requester_email, :status, :current_content, presence: true
+  validate :content_data_must_be_string_pairs
+
   scope :for_source, ->(source_id) { where(source_id: source_id) }
   scope :most_recent_first, -> { order(created_at: :desc) }
   scope :most_recent_for_source, ->(source_id) { for_source(source_id).most_recent_first.first }
+
+  def content_data_must_be_string_pairs
+    fields_to_test = %i[current_content previous_content].select { |f| self[f].present? }
+
+    fields_to_test.each do |content_field|
+      content_hash = public_send(content_field)
+      unless content_hash.is_a?(Hash)
+        errors.add(content_field, "#{content_field} is not a hash")
+        next
+      end
+
+      content_hash.each do |key, value|
+        unless value.is_a?(String)
+          errors.add(content_field, "value for #{key} must be a string")
+        end
+      end
+    end
+  end
 end
