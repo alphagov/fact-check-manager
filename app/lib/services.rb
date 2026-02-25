380df9ff16d636785767fe51f15a97153816ab43
Alex Sheppard <alexander.sheppard@digital.cabinet-office.gov.uk>
2026-02-25 16:34:00 +0000

    Extend GdsApi with adapter method for Publisher, and initialise Publisher as a service

---
+++ app/lib/services.rb
@@ -0,0 +1,8 @@
+module Services
+  def self.publisher_api
+    @publisher ||= GdsApi::Publisher.new(
+      Plek.find("publisher"),
+      bearer_token: ENV.fetch("PUBLISHER_BEARER_TOKEN", "example"),
+      )
+  end
+end
