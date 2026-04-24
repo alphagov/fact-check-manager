ec51b2da3044496571a316cc7be50fcd0e0be5b7
Helen Pickavance <helen.pickavance@digital.cabinet-office.gov.uk>
2026-04-24 16:36:26 +0100

    Add notify_api_service tests

---
+++ spec/services/notify_api_service_spec.rb
@@ -0,0 +1,28 @@
+require "rails_helper"
+require "notifications/client"
+
+RSpec.describe NotifyApiService do
+  before do
+    @request = build(:request)
+    @user = create(:user, name: "Douglas Adams")
+    @service = described_class
+    @personalisation_hash = { greeting: "Hola, Mundo!!" }
+    @notify_client_spy = instance_spy(Notifications::Client)
+    allow(Services).to receive(:notify_api).and_return(@notify_client_spy)
+  end
+
+  context "sending individual emails" do
+    it "sends the provided data to Notify" do
+      @service.send_email_to_recipient(@user, @request, NotifyApiService::NOTIFY_TEST_TEMPLATE_ID, @personalisation_hash)
+
+      expect(@notify_client_spy).to have_received(:send_email).with(
+        hash_including(
+          email_address: @user.email,
+          template_id: NotifyApiService::NOTIFY_TEST_TEMPLATE_ID,
+          reference: "#{@request.source_app}/#{@request.source_id}",
+          personalisation: @personalisation_hash,
+        ),
+      )
+    end
+  end
+end
