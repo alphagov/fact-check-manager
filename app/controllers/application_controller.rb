2e7075095d8cb6737444fbac8e60f2afd1cac622
Helen Pickavance <helen.pickavance@digital.cabinet-office.gov.uk>
2026-04-24 16:39:50 +0100

    Add error handling for GOV.UK Notify exceptions at the ApplicationController level

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
+require "notifications/client"
+
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   layout "design_system"
 
   include GDS::SSO::ControllerMethods
   include TokenHelper
+
+  rescue_from Notifications::Client::RequestError, with: :notify_request_error_handler
+  rescue_from Notifications::Client::BadRequestError, with: :notify_bad_request_error_handler
 
   before_action :authenticate_user!, unless: :token_bypass?
 
@@ -24,5 +29,26 @@ class ApplicationController < ActionController::Base
 
   def preview_params
     params.permit(:source_app, :source_id, :token)
+  end
+
+  def notify_request_error_handler(exception)
+    Rails.logger.info("Error: #{exception.code}, #{exception.message}")
+    render json: { errors: { notify_error: exception.message, error_code: exception.code } }, status: :bad_gateway
+  end
+
+  def notify_bad_request_error_handler(exception)
+    not_prod = %w[integration staging].include?(ENV.fetch("GOVUK_ENVIRONMENT"))
+    if not_prod && exception.message =~ /team-only API key/
+      team_only_error_message = "One or more recipients not in GOV.UK Notify team. This error will not occur in Production."
+      Rails.logger.info("Error: #{exception.code}, #{team_only_error_message}")
+      render json: {
+        errors: {
+          notify_error: team_only_error_message,
+          error_code: exception.code,
+        },
+      }, status: :bad_gateway
+    else
+      notify_request_error_handler(exception)
+    end
   end
 end
