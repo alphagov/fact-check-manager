5278afb6a2e82a422ca525d90838cb9294b7162e
David Trussler <david.trussler@digital.cabinet-office.gov.uk>
2026-06-26 17:11:13 +0100

    Add `ga4-index-section-setup` module to app

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,6 +4,7 @@
 //= require govuk_publishing_components/lib
 //= require govuk_publishing_components/analytics-ga4/ga4-form-change-tracker
 //= require analytics_modules/ga4-form-setup
+//= require analytics_modules/ga4-index-section-setup
 
 window.GOVUK.approveAllCookieTypes()
 window.GOVUK.cookie('cookies_preferences_set', 'true', { days: 365 })
