ab8cdf4b65aefec0e54b0695b36bfa2093dc86b6
David Trussler <david.trussler@digital.cabinet-office.gov.uk>
2026-07-20 17:22:08 +0100

    Update `ga4-links-setup.spec.js` - test for new orientation of links on the page (less global) - revert module to page wide and operating on each module separately - Tidy - for linting

--- spec/javascripts/spec/ga4-links-setup.spec.js
+++ spec/javascripts/spec/ga4-links-setup.spec.js
@@ -1,58 +1,30 @@
 describe('GA4LinksSetup', function () {
   'use strict'
 
-  var module, ga4LinksSetup
-
-  beforeEach(function () {
-    var moduleHtml =
-      `<header data-module="ga4-links-setup">
-          <a id="rootLink">Fact Check Manager</a>
-          <a id="signoutLink">Sign out</a>
-        </header>
-        <main data-module="ga4-links-setup">
-          <div>
-            <div class="gem-c-heading">
-              <h1>The page title</h1>
-            </div>
-          </div>
-          <div>
-            <p>Some content</p>
-            <a id="bodyLink_1">The first link</a>
-          </div>
-          <div>
-            <div class="gem-c-heading">
-              <h2>The first section heading</h2>
-            </div>
-            <p>Some more content</p>
-            <a id="bodyLink_2">The second link</a>
-            <div class="gem-c-heading">
-              <h2>The second section heading</h2>
-            </div>
-            <p>Even more content</p>
-            <a id="bodyLink_3">The third link</a>
-            <a id="bodyLink_4">The fourth link</a>
-          </div>
-        </main>`
-
-    module = document.createElement('div')
-    module.innerHTML = moduleHtml
-    document.body.appendChild(module)
-
-    ga4LinksSetup = new window.GOVUK.Modules.Ga4LinksSetup()
-    ga4LinksSetup.init()
-  })
+  var module, moduleHtml, ga4LinksSetup
 
   afterEach(function () {
     document.body.removeChild(module)
   })
 
   describe('when loaded', function () {
-    it('adds the correct parameters to the links in the header', function () {
-      var header = module.querySelector('header')
-      var rootLink = header.querySelector('a#rootLink')
-      var signoutLink = header.querySelector('a#signoutLink')
-      var rootLinkGA4Data = JSON.parse(rootLink.dataset.ga4Link)
-      var signoutLinkGA4Data = JSON.parse(signoutLink.dataset.ga4Link)
+    it('adds the correct parameters to the links in the header section', function () {
+      var rootLink, signoutLink, rootLinkGA4Data, signoutLinkGA4Data
+
+      moduleHtml = `
+        <a id="rootLink">Fact Check Manager</a>
+        <a id="signoutLink">Sign out</a>`
+      module = document.createElement('header')
+      module.innerHTML = moduleHtml
+      document.body.appendChild(module)
+
+      ga4LinksSetup = new window.GOVUK.Modules.Ga4LinksSetup(module)
+      ga4LinksSetup.init()
+
+      rootLink = module.querySelector('a#rootLink')
+      signoutLink = module.querySelector('a#signoutLink')
+      rootLinkGA4Data = JSON.parse(rootLink.dataset.ga4Link)
+      signoutLinkGA4Data = JSON.parse(signoutLink.dataset.ga4Link)
 
       expect(rootLinkGA4Data.event_name).toBe('navigation')
       expect(rootLinkGA4Data.type).toBe('header')
@@ -72,39 +44,41 @@ describe('GA4LinksSetup', function () {
     })
 
     it('adds the correct parameters to the links in the main section', function () {
-      var main = module.querySelector('main')
-      var bodyLink_1 = main.querySelector('a#bodyLink_1')
-      var bodyLink_2 = main.querySelector('a#bodyLink_2')
-      var bodyLink_3 = main.querySelector('a#bodyLink_3')
-      var bodyLink_4 = main.querySelector('a#bodyLink_4')
-      var bodyLink_1_GA4Data = JSON.parse(bodyLink_1.dataset.ga4Link)
-      var bodyLink_2_GA4Data = JSON.parse(bodyLink_2.dataset.ga4Link)
-      var bodyLink_3_GA4Data = JSON.parse(bodyLink_3.dataset.ga4Link)
-      var bodyLink_4_GA4Data = JSON.parse(bodyLink_4.dataset.ga4Link)
+      var bodyLink1, bodyLink2, bodyLink3, bodyLink1GA4Data, bodyLink2GA4Data, bodyLink3GA4Data
 
-      expect(bodyLink_1_GA4Data.event_name).toBe('navigation')
-      expect(bodyLink_1_GA4Data.type).toBe('generic_link')
-      // expect(bodyLink_1_GA4Data.index_link).toBe('1')
-      // expect(bodyLink_1_GA4Data.index_section).toBe('1')
-      // expect(bodyLink_1_GA4Data.index_section_count).toBe('2')
-      // expect(bodyLink_1_GA4Data.index_total).toBe('2')
-      expect(bodyLink_1_GA4Data.section).toBe('')
+      moduleHtml =
+        `<div>
+          <p>Some content</p>
+          <a id="bodyLink_1">The first link</a>
+        </div>
+        <div>
+          <p>Some more content</p>
+          <a id="bodyLink_2">The second link</a>
+          <p>Even more content</p>
+          <a id="bodyLink_3">The third link</a>
+        </div>`
+      module = document.createElement('main')
+      module.innerHTML = moduleHtml
+      document.body.appendChild(module)
 
-      expect(bodyLink_2_GA4Data.event_name).toBe('navigation')
-      expect(bodyLink_2_GA4Data.type).toBe('generic_link')
-      // expect(bodyLink_2_GA4Data.index_link).toBe('1')
-      // expect(bodyLink_2_GA4Data.index_section).toBe('1')
-      // expect(bodyLink_2_GA4Data.index_section_count).toBe('2')
-      // expect(bodyLink_2_GA4Data.index_total).toBe('2')
-      expect(bodyLink_2_GA4Data.section).toBe('The first section heading')
+      ga4LinksSetup = new window.GOVUK.Modules.Ga4LinksSetup(module)
+      ga4LinksSetup.init()
 
-      expect(bodyLink_3_GA4Data.event_name).toBe('navigation')
-      expect(bodyLink_3_GA4Data.type).toBe('generic_link')
-      // expect(bodyLink_3_GA4Data.index_link).toBe('1')
-      // expect(bodyLink_3_GA4Data.index_section).toBe('1')
-      // expect(bodyLink_3_GA4Data.index_section_count).toBe('2')
-      // expect(bodyLink_3_GA4Data.index_total).toBe('2')
-      expect(bodyLink_3_GA4Data.section).toBe('The second section heading')
+      bodyLink1 = module.querySelector('a#bodyLink_1')
+      bodyLink2 = module.querySelector('a#bodyLink_2')
+      bodyLink3 = module.querySelector('a#bodyLink_3')
+      bodyLink1GA4Data = JSON.parse(bodyLink1.dataset.ga4Link)
+      bodyLink2GA4Data = JSON.parse(bodyLink2.dataset.ga4Link)
+      bodyLink3GA4Data = JSON.parse(bodyLink3.dataset.ga4Link)
+
+      expect(bodyLink1GA4Data.event_name).toBe('navigation')
+      expect(bodyLink1GA4Data.type).toBe('generic_link')
+
+      expect(bodyLink2GA4Data.event_name).toBe('navigation')
+      expect(bodyLink2GA4Data.type).toBe('generic_link')
+
+      expect(bodyLink3GA4Data.event_name).toBe('navigation')
+      expect(bodyLink3GA4Data.type).toBe('generic_link')
     })
   })
 })
