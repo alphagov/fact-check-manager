16bbf38f2eb6a7a88ed9424f661553c8600af221
Helen Pickavance <Helen.pickavance@dsit.gov.uk>
2026-09-09 17:17:08 +0100

    Validate that body has max length of 9000 chars

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,4 +1,6 @@
 class Response < ApplicationRecord
+  BODY_MAX_LENGTH = 9000
+
   belongs_to :request
   belongs_to :user
 
@@ -6,6 +8,7 @@ class Response < ApplicationRecord
             presence: {
               message: "cannot be blank if accepted is false",
             }, unless: :accepted
+  validates :body, length: { maximum: BODY_MAX_LENGTH, message: "length must be a maximum of %{count} characters" }
   validates :request_id, uniqueness: { message: "has already been responded to" }
   validates :accepted, inclusion: { in: [true, false], message: "must be true or false" }
 end
