8c999ad9a7e83f84cd7c5c324bb19a8c577b0d54
Helen Pickavance <Helen.pickavance@dsit.gov.uk>
2026-09-09 11:57:31 +0100

    Validate at API endpoint that deadline is a valid date
    - Prior to any 'magic' Rails casting, this checks that the posted string is a valid datetime

--- app/controllers/api/requests_controller.rb
+++ app/controllers/api/requests_controller.rb
@@ -78,8 +78,9 @@ module Api
 
     def validate_create_params
       errors = []
-
       errors << "At least one recipient email is required" if request_params[:recipients].blank?
+      deadline = request_params[:deadline]
+      errors << "Deadline must be a valid datetime string" unless deadline.is_a?(String) && Time.zone.parse(deadline)
 
       %i[current_content previous_content].each do |content_hash|
         if params.dig(:request, content_hash).present? && request_params[content_hash].blank?
